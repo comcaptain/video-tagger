facf4168fe48885603cc417bd3eadb484f316b37
Tony <htmlcaptain@gmail.com>
2019-12-08 16:12:05 +0900

    Add css

--- src/CreateNewTag.js
+++ src/CreateNewTag.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './CreateNewTag.css';
 
 export default class CreateNewTag extends React.Component {
 	constructor(props) {
@@ -14,9 +15,14 @@ export default class CreateNewTag extends React.Component {
 	render() {
 		return (
 			<div id="create-new-tag">
-				<div id="video-file-path">{this.state.videoFilePath}</div>
-				<div id="seek-position">{this.state.seekPosition}</div>
-				<img src={"file:///" + this.state.screenshotFilePath} alt={this.state.screenshotFilePath}/>
+				<div id="meta-data">
+					<span id="video-file-path">{this.state.videoFilePath}</span>
+					<span id="seek-position">{this.state.seekPosition}</span>
+				</div>
+				<img 
+					id="video-screenshot"
+					src={"file:///" + this.state.screenshotFilePath} 
+					alt={this.state.screenshotFilePath}/>
 			</div>
 		)
 	}
