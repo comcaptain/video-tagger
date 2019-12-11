d34018634e847d0cb1b5b06803dffb8f4dc3e8df
Tony <htmlcaptain@gmail.com>
2019-12-11 21:56:42 +0900

    Do fingerprint calculation for all video files
    It's surprisingly fast!
    
    Scanned 539 directories, 10484 files, 847 video files and indexed 847 video files in 00:00:06.7

--- src/scanner/VideoScanner.js
+++ src/scanner/VideoScanner.js
@@ -2,18 +2,36 @@ const fs = require("fs")
 const path = require("path")
 const util = require('util');
 const fsReadDir = util.promisify(fs.readdir);
-const VIDEO_EXTENSIONS = new Set([".mp4", ".wmv", ".mkv", ".avi", ".rmvb", ".mp3", ".rm", ".flv", ".mov", ".3gp", ".VOB", ".MKV"]);
+const FingerprintCalculator = require('../share/FingerprintCalculator.js')
+
+const VIDEO_EXTENSIONS = new Set([".mp4", ".wmv", ".mkv", ".avi", ".rmvb", ".rm", ".flv", ".mov", ".3gp", ".VOB", ".MKV"]);
+
+function msToTime(duration) {
+  var milliseconds = parseInt((duration % 1000) / 100),
+    seconds = Math.floor((duration / 1000) % 60),
+    minutes = Math.floor((duration / (1000 * 60)) % 60),
+    hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+
+  hours = (hours < 10) ? "0" + hours : hours;
+  minutes = (minutes < 10) ? "0" + minutes : minutes;
+  seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+  return hours + ":" + minutes + ":" + seconds + "." + milliseconds;
+}
+
 class VideoScanner {
 	constructor(...directories) {
 		this._directories = directories;
 		this._scannedDirectoryCount = 0;
 		this._scannedFileCount = 0;
 		this._scannedVideoFileCount = 0;
+		this._indexedVideoFileCount = 0;
 	}
 
 	scan() {
+		this._startTime = new Date().getTime();
 		return Promise.all(this._directories.map(this.scanDirectory, this)).then(() => {
-			console.info(`Scanned ${this._scannedDirectoryCount} directories, ${this._scannedFileCount} files and ${this._scannedVideoFileCount} video files`)
+			console.info(`Scanned ${this._scannedDirectoryCount} directories, ${this._scannedFileCount} files, ${this._scannedVideoFileCount} video files and indexed ${this._indexedVideoFileCount} video files in ${msToTime(new Date().getTime() - this._startTime)}`)
 		})
 	}
 
@@ -23,12 +41,18 @@ class VideoScanner {
 			if (file.isDirectory()) {
 				return this.scanDirectory(fullPath);
 			}
-			if (file.isFile()) {
-				let extension = path.extname(file.name);
-				if (VIDEO_EXTENSIONS.has(extension)) this._scannedVideoFileCount++;
-				this._scannedFileCount++;
-			}
-			return Promise.resolve();
+			let resolvedPromise = Promise.resolve();
+			if (!file.isFile()) return resolvedPromise;
+
+			let extension = path.extname(file.name);
+			this._scannedFileCount++;
+			if (!VIDEO_EXTENSIONS.has(extension)) return resolvedPromise;
+
+			this._scannedVideoFileCount++;
+			return new FingerprintCalculator(fullPath).calculate().then(fingerprint => {
+				this._indexedVideoFileCount++;
+				console.info(`${msToTime(new Date().getTime() - this._startTime)} [${this._indexedVideoFileCount} / ${this._scannedVideoFileCount}] Fingerprint of video file ${fullPath} is ${fingerprint}`);
+			})
 		}))).then(() => this._scannedDirectoryCount++)
 	}
 }
