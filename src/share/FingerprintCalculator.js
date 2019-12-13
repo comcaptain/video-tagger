3e1ef9aa246b01cc9ecff1e9a545932ff3703374
Tony <htmlcaptain@gmail.com>
2019-12-14 00:18:17 +0900

    Improve the algorithm to make collision happen less frequently

--- src/share/FingerprintCalculator.js
+++ src/share/FingerprintCalculator.js
@@ -3,7 +3,8 @@ const util = require('util');
 const fsOpen = util.promisify(fs.open);
 const fsRead = util.promisify(fs.read);
 const fsStat = util.promisify(fs.stat);
-const SAMPLE_BYTES_COUNT = 10;
+const SAMPLE_BYTES_COUNT = 20;
+const SAMPLE_OFFSET = 1024 * 1024 * 20;
 
 module.exports = class FingerprintCalculator {
 	constructor(filePath) {
@@ -13,15 +14,17 @@ module.exports = class FingerprintCalculator {
 	calculate() {
 		return fsOpen(this._filePath, 'r').then(fd => {
 			let buffer = Buffer.alloc(SAMPLE_BYTES_COUNT * 3);
-			let promises = [fsRead(fd, buffer, 0, SAMPLE_BYTES_COUNT, null)];
+			let promises = [];
 			return fsStat(this._filePath).then(stats => {
 				let size = stats.size;
 				if (size < SAMPLE_BYTES_COUNT * 3) {
 					throw `Size of ${this._filePath} is ${size}, smaller than minimum size ${SAMPLE_BYTES_COUNT * 3}`
 				}
+				let offset = Math.min(SAMPLE_OFFSET, (size - SAMPLE_BYTES_COUNT * 3) / 2);
 				let middlePosition = size / 2 - SAMPLE_BYTES_COUNT / 2;
+				promises.push(fsRead(fd, buffer, 0, SAMPLE_BYTES_COUNT, offset));
 				promises.push(fsRead(fd, buffer, SAMPLE_BYTES_COUNT, SAMPLE_BYTES_COUNT, middlePosition));
-				promises.push(fsRead(fd, buffer, SAMPLE_BYTES_COUNT * 2, SAMPLE_BYTES_COUNT, size - SAMPLE_BYTES_COUNT));
+				promises.push(fsRead(fd, buffer, SAMPLE_BYTES_COUNT * 2, SAMPLE_BYTES_COUNT, size - SAMPLE_BYTES_COUNT - offset));
 				return Promise.all(promises).then(v => buffer.toString("base64"))
 			})
 		})
