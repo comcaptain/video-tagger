702eb124a28d74b62929bdc2bd936a112370c30a
Tony <htmlcaptain@gmail.com>
2019-12-14 02:42:36 +0900

    change ctrl+click to double click

--- src/react/video/Thumnail.js
+++ src/react/video/Thumnail.js
@@ -4,18 +4,16 @@ const VideoPlayer = require('./VideoPlayer.js');
 
 export default class Thumnail extends React.Component {
 
-	handleClick(event) {
-		if (event.ctrlKey) {
-			let videoPlayer = new VideoPlayer(this.props.videoPath, this.props.seekPosition);
-			videoPlayer.play();
-		}
+	handleDoubleClick(event) {
+		let videoPlayer = new VideoPlayer(this.props.videoPath, this.props.seekPosition);
+		videoPlayer.play();
 	}
 
 	render() {
 		return (
 			<img 
 				className="thumnail" 
-				onClick={this.handleClick.bind(this)}
+				onDoubleClick={this.handleDoubleClick.bind(this)}
 				src={this.props.screenshotPath} 
 				alt={this.props.screenshotPath}
 				/>
