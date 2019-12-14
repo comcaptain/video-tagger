08f964664137415d107961aab871921508ae5e63
Tony <htmlcaptain@gmail.com>
2019-12-15 01:30:00 +0900

    Backup screenshots

--- electron/backup/DataBackuper.js
+++ electron/backup/DataBackuper.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const util = require('util');
 const fsMkdir = util.promisify(fs.mkdir);
+const fse = require('fs-extra')
+const fseCopy = util.promisify(fse.copy);
 const path = require('path');
 const formatDate = require('dateformat');
 const exec = util.promisify(require('child_process').exec);
@@ -13,7 +15,15 @@ class DataBackuper {
 
 	async backup() {
 		await fsMkdir(this._backupDirectory, {recursive: true});
-		return Promise.all([this.backupDB()])
+		return Promise.all([this.backupDB(), this.backupScreenshots()]);
+	}
+
+	async backupScreenshots() {
+		let fromDirectory = conf.screenshot_directory;
+		let toDirectory = path.join(this._backupDirectory, "screenshots");
+		console.log(`Copying screenshots from ${fromDirectory} to ${toDirectory} ...`);
+		await fseCopy(fromDirectory, toDirectory);
+		console.log(`Copied screenshots from ${fromDirectory} to ${toDirectory}`);
 	}
 
 	async backupDB() {
