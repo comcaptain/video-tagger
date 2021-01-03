4295f91df37c80c922a874f62af0b754d738bede
Tony <htmlcaptain@gmail.com>
2021-01-03 22:34:19 +0900

    Shuffle the video list before displaying and only display the 1st 20 videos

--- src/react/video/VideoList.tsx
+++ src/react/video/VideoList.tsx
@@ -13,10 +13,33 @@ interface State {
 	screenshotsPerLine: number;
 }
 
+function shuffle(array: any[]): any[] {
+	array = array.slice();
+
+	var currentIndex = array.length, temporaryValue, randomIndex;
+
+	// While there remain elements to shuffle...
+	while (0 !== currentIndex) {
+
+		// Pick a remaining element...
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+
+		// And swap it with the current element.
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
+
+	return array;
+}
+
+
+
 export default class VideoList extends React.Component<Props, State> {
 	constructor(props: Props) {
 		super(props);
-		this.state = {screenshotsPerLine: 4};
+		this.state = { screenshotsPerLine: 4 };
 		this.updateScreenshotsPerLine = this.updateScreenshotsPerLine.bind(this);
 	}
 
@@ -27,17 +50,17 @@ export default class VideoList extends React.Component<Props, State> {
 	}
 
 	render() {
-		let videos = this.props.videos;
-        let thumbnailStyle = {width: `calc(${100 / this.state.screenshotsPerLine}% - 1px)`}
+		let videos = shuffle(this.props.videos).slice(0, 20);
+		let thumbnailStyle = { width: `calc(${100 / this.state.screenshotsPerLine}% - 1px)` }
 		let videoDOMs = videos.map(video => <Video {...video} collapsedByDefault={this.props.collapsedByDefault} key={video.path} thumbnailStyle={thumbnailStyle} />);
 		return (<div>
 			<Navigation name="list" />
 			<div id="videos-container">
 				<div id="videos-meta">
 					<span id="video-count">{videos.length}个视频</span>
-					<input type="number" 
-						id="screenshots-per-line" 
-						onChange={this.updateScreenshotsPerLine} 
+					<input type="number"
+						id="screenshots-per-line"
+						onChange={this.updateScreenshotsPerLine}
 						value={this.state.screenshotsPerLine} />
 					{this.props.children}
 				</div>
